5a80997c35f08707ac0854f9bf4d47b307fcb69d
Louis Heal <lh420@ic.ac.uk>
2022-10-22 00:01:53 +0100

    [lh420 av1620] feat: implement constructor, add, contains and remove functions for sequential hash set

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -2,33 +2,53 @@
 #define HASH_SET_SEQUENTIAL_H
 
 #include <cassert>
+#include <functional>
+#include <set>
+#include <vector>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetSequential : public HashSetBase<T> {
  public:
-  explicit HashSetSequential(size_t /*initial_capacity*/) {}
-
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  explicit HashSetSequential(size_t initial_capacity) : capacity_(initial_capacity) {
+    std::vector<std::set<T>> table_(capacity_);
+    size_t size_ = 0;
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Add(T elem) final {
+    size_t bucketNum = std::hash<T>()(elem) % capacity_;
+    std::set<T> bucket = table_[bucketNum];
+
+    result = bucket.insert(elem);
+    size_ += result ? 1 : 0;
+
+    return result;
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool Remove(T elem) final {
+    size_t bucketNum = std::hash<T>()(elem) % capacity_;
+    std::set<T> bucket = table_[bucketNum];
+
+    result = bucket.remove(elem);
+    size_ += result ? 1 : 0;
+
+    return result;
+  }
+
+  [[nodiscard]] bool Contains(T elem) final {
+    size_t bucketNum = std::hash<T>()(elem) % capacity_;
+    set<T> bucket = table_.[bucketNum];
+
+    return bucket.find(elem) != bucket.end();
   }
 
   [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+    return self.size_;
   }
+
+  private:
+    size_t capacity_;
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
