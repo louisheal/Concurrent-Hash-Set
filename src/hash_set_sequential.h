02caac97d0816620c8f2318dd4c2511f3b981374
Andriy Vervega <anjey@Andriys-MacBook-Pro.local>
2022-10-22 11:25:08 +0100

    fix: semi-fix of the sequential hash

--- src/hash_set_sequential.h
+++ src/hash_set_sequential.h
@@ -8,47 +8,77 @@
 
 #include "src/hash_set_base.h"
 
-template <typename T>
+template<typename T>
 class HashSetSequential : public HashSetBase<T> {
- public:
-  explicit HashSetSequential(size_t initial_capacity) : capacity_(initial_capacity) {
-    std::vector<std::set<T>> table_(capacity_);
-    size_t size_ = 0;
-  }
+public:
+    explicit HashSetSequential(size_t initial_capacity) : capacity_(initial_capacity) {
+        table_ = std::vector<std::set<T>>(initial_capacity);
+        size_ = 0;
+    }
 
-  bool Add(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-    std::set<T> bucket = table_[bucketNum];
+    explicit HashSetSequential() : capacity_(16) {
+        table_ = std::vector<std::set<T>>(16);
+        size_ = 0;
+    }
 
-    result = bucket.insert(elem);
-    size_ += result ? 1 : 0;
+    bool Add(T elem) override {
+        size_t bucketNum = std::hash<T>()(elem) % capacity_;
+        std::set<T> bucket = table_[bucketNum];
+        auto result = bucket.insert(elem);
+        size_ += result.second ? 1 : 0;
+        if (policy()) {
+            resize();
+        }
 
-    return result;
-  }
+        return result.second;
+    }
 
-  bool Remove(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-    std::set<T> bucket = table_[bucketNum];
+    bool Remove(T elem) override {
+        size_t bucketNum = std::hash<T>()(elem) % capacity_;
+        std::set<T> bucket = table_[bucketNum];
 
-    result = bucket.remove(elem);
-    size_ += result ? 1 : 0;
+        bool result = bucket.erase(elem);
+        size_ -= result ? 1 : 0;
 
-    return result;
-  }
+        return result;
+    }
 
-  [[nodiscard]] bool Contains(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-    set<T> bucket = table_.[bucketNum];
+    [[nodiscard]] bool Contains(T elem) final override{
+        size_t bucketNum = std::hash<T>()(elem) % capacity_;
+        std::set<T> bucket = table_[bucketNum];
 
-    return bucket.find(elem) != bucket.end();
-  }
+        return bucket.find(elem) != bucket.end();
+    }
 
-  [[nodiscard]] size_t Size() const final {
-    return self.size_;
-  }
+    [[nodiscard]] size_t Size() const final override{
+        return size_;
+    }
 
-  private:
+private:
+    bool policy() {
+        return size_ / table_.size() > 4;
+    }
+
+    void resize() {
+        int old_capacity = table_.size();
+        // This assignment creates a copy of the table.
+        // Play with references here
+        std::vector<std::set<T>> old_table = table_;
+        table_ = std::vector<std::set<T>>(capacity_);
+        for (int i = 0; i < old_capacity; i++) {
+            std::set<T> bucket = old_table[i];
+            for (auto& elem : bucket) {
+                size_t new_bucket_num = bucket[std::hash<T>()(elem) % table_.size()];
+                table_[new_bucket_num].insert(elem);
+            }
+        }
+    }
+
+private:
+    std::vector<std::set<T>> table_;
     size_t capacity_;
+    size_t size_;
+
 };
 
 #endif  // HASH_SET_SEQUENTIAL_H
