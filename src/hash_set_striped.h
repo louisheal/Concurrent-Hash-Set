19fb61a427f89ba004398d9d50163bb5ac5fddd5
Louis Heal <lh420@ic.ac.uk>
2022-10-24 18:43:36 +0100

    feat: implement striped hash set

--- src/hash_set_striped.h
+++ src/hash_set_striped.h
@@ -2,33 +2,112 @@
 #define HASH_SET_STRIPED_H
 
 #include <cassert>
+#include <functional>
+#include <mutex>
+#include <set>
+#include <vector>
+#include <atomic>
 
 #include "src/hash_set_base.h"
 
 template <typename T>
 class HashSetStriped : public HashSetBase<T> {
  public:
-  explicit HashSetStriped(size_t /*initial_capacity*/) {}
+  explicit HashSetStriped(size_t initial_capacity) : size_(0), table_(initial_capacity), locks_(initial_capacity) {}
 
-  bool Add(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+    bool Add(T elem) final {
+      Acquire(elem);
+      size_t bucketNum = std::hash<T>()(elem) % table_.size();
+      bool result = table_[bucketNum].insert(elem).second;
+      Release(elem);
+      if (result) {
+        size_.fetch_add(1);
+      }
+      if (policy()) {
+        resize();
+      }
+      return result;
+    }
+
+    bool Remove(T elem) final {
+      Acquire(elem);
+      size_t hash = std::hash<T>()(elem);
+      bool result = table_[hash % table_.size()].erase(elem);
+      Release(elem);
+      if (result) {
+        size_.fetch_sub(1);
+      }
+      return result;
+    }
+
+    [[nodiscard]] bool Contains(T elem) final {
+      Acquire(elem);
+      size_t bucketNum = std::hash<T>()(elem) % table_.size();
+      bool result = table_[bucketNum].find(elem) != table_[bucketNum].end();
+      Release(elem);
+      return result;
+    }
+
+    [[nodiscard]] size_t Size() const final {
+      return static_cast<size_t>(size_.load());
+    }
+
+private:
+  void Acquire(T elem) {
+    size_t hash_code = std::hash<T>()(elem);
+    locks_[hash_code % locks_.size()].lock();
   }
 
-  bool Remove(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  void Release(T elem) {
+    size_t hash_code = std::hash<T>()(elem);
+    locks_[hash_code % locks_.size()].unlock();
   }
 
-  [[nodiscard]] bool Contains(T /*elem*/) final {
-    assert(false && "Not implemented yet");
-    return false;
+  bool policy() {
+    return static_cast<size_t>(size_.load()) / table_.size() > 4;
   }
 
-  [[nodiscard]] size_t Size() const final {
-    assert(false && "Not implemented yet");
-    return 0u;
+  void resize() {
+
+    size_t old_capacity = table_.size();
+
+    for (auto& lock : locks_) {
+      lock.lock();
+    }
+
+    if (old_capacity != table_.size()) {
+      for (auto& lock : locks_) {
+        lock.unlock();
+      }
+      return;
+    }
+
+    size_t new_capacity = 2 * old_capacity;
+
+    std::vector<std::set<int>> old_table;
+    old_table.assign(table_.begin(), table_.end());
+
+    table_.clear(); table_.resize(new_capacity);
+
+    for (std::set<int> &bucket: old_table) {
+      for (int elem: bucket) {
+        size_t bucket_num = std::hash<int>()(elem) % new_capacity;
+        table_[bucket_num].insert(elem);
+      }
+    }
+
+    for (auto& lock : locks_) {
+      lock.unlock();
+    }
   }
+
+private:
+  //  Number of elements in the hashset
+  std::atomic<int> size_;
+  //  Vector of buckets for elements
+  std::vector<std::set<T>> table_;
+  //  Vector of mutexes for each stripe
+  std::vector<std::mutex> locks_;
 };
 
 #endif  // HASH_SET_STRIPED_H
