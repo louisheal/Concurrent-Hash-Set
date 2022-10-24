977cda3342810e5eb48286bcc942f2ffc8955763
Louis Heal <lh420@ic.ac.uk>
2022-10-24 15:52:54 +0100

    feat: implement coarse grained set with resizing

--- src/hash_set_coarse_grained.h
+++ src/hash_set_coarse_grained.h
@@ -12,42 +12,32 @@
 template<typename T>
 class HashSetCoarseGrained : public HashSetBase<T> {
   public:
-  explicit HashSetCoarseGrained(size_t initial_capacity) : capacity_(initial_capacity),
-                                                           table_(capacity_, std::set<T>()) {
-    table_.resize(capacity_);
-    size_ = 0;
+  explicit HashSetCoarseGrained(size_t initial_capacity) : size_(0), table_(initial_capacity) {
   }
 
   bool Add(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-
-    mutex_.lock();
+    std::scoped_lock<std::mutex> lock(mutex_);
+    size_t bucketNum = std::hash<T>()(elem) % table_.size();
     auto result = table_[bucketNum].insert(elem).second;
     size_ += result ? 1 : 0;
-    mutex_.unlock();
-
+    if (policy()) {
+      resize();
+    }
     return result;
   }
 
   bool Remove(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-
-    mutex_.lock();
+    std::scoped_lock<std::mutex> lock(mutex_);
+    size_t bucketNum = std::hash<T>()(elem) % table_.size();
     bool result = table_[bucketNum].erase(elem);
     size_ -= result ? 1 : 0;
-    mutex_.unlock();
-
     return result;
   }
 
   [[nodiscard]] bool Contains(T elem) final {
-    size_t bucketNum = std::hash<T>()(elem) % capacity_;
-
-    mutex_.lock();
-    size_t result = table_[bucketNum].count(elem);
-    mutex_.unlock();
-
-    return result == 1;
+    std::scoped_lock<std::mutex> lock(mutex_);
+    size_t bucketNum = std::hash<T>()(elem) % table_.size();
+    return table_[bucketNum].count(elem) == 1;
   }
 
   [[nodiscard]] size_t Size() const final {
@@ -55,10 +45,38 @@ class HashSetCoarseGrained : public HashSetBase<T> {
   }
 
   private:
-  size_t capacity_;
-  size_t size_;
-  std::vector<std::set<T>> table_;
-  std::mutex mutex_;
+
+    // Checking whether we need to resize the hashset to guarantee const time operations
+    bool policy() {
+      return size_ / table_.size() > 4;
+    }
+
+    void resize() {
+      size_t old_capacity = table_.size();
+
+      if (old_capacity != table_.size()) {
+        return;
+      }
+
+      size_t new_capacity = 2 * old_capacity;
+
+      std::vector<std::set<int>> old_table;
+      old_table.assign(table_.begin(), table_.end());
+
+      table_.clear(); table_.resize(new_capacity);
+
+      for (std::set<int> &bucket: old_table) {
+        for (int elem: bucket) {
+          size_t bucket_num = std::hash<int>()(elem) % new_capacity;
+          table_[bucket_num].insert(elem);
+        }
+      }
+    }
+
+  private:
+    size_t size_;
+    std::vector<std::set<T>> table_;
+    std::mutex mutex_;
 };
 
 #endif  // HASH_SET_COARSE_GRAINED_H
